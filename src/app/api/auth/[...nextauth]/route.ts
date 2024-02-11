3b303d02bb9bd089710628eee9439368e87e4e6d
MrEpoch <alexandrstencuk@seznam.cz>
2024-02-11 19:39:11 +0100

    New school project

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { prisma } from "@/lib/db";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER, 
+      from: process.env.EMAIL_FROM,
+    })
+  ]
+})
+
+export { handler as GET, handler as POST }
